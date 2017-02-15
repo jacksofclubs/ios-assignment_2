a623e546cafd62bfedf3a021d8b1d28dc6816218
Kevin Jacks <kevinjackswi@gmail.com>
2017-02-15 16:18:50 -0600

    Initial Commit

---
+++ assignment_2_v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment_2_v2
+//
+//  Created by Kevin Jacks on 2/15/17.
+//  Copyright © 2017 Kevin Jacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
