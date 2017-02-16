9f5a8daa7f3365e77892aa90da55cfaaa7f91907
Kevin Jacks <kevinjackswi@gmail.com>
2017-02-15 21:19:44 -0600

    buttons change label text

--- assignment_2_v2/ViewController.swift
+++ assignment_2_v2/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var greetingText: UILabel!
+    
+    @IBAction func iJustGotHere(_ sender: UIButton) {
+        greetingText.text = "Hello"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func imLeaving(_ sender: UIButton) {
+        greetingText.text = "Goodbye"
     }
-
 
 }
 
